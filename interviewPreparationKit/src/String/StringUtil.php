55dc4d07f56bd45362ba2ffde62b230ad6b55582
samueldcabral <samueldcabral@gmail.com>
2019-04-20 20:29:06 -0300

    final exercises done

--- interviewPreparationKit/src/String/StringUtil.php
+++ interviewPreparationKit/src/String/StringUtil.php
@@ -7,20 +7,71 @@ class StringUtil
   public static function repeatedString($s, $n): int
   {
     // TODO
+    $aCount = 0;
+    for($x = 0; $x < strlen($s); $x++) {
+      if($s[$x] === "a") {
+        $aCount++;
+      }
+    }
+
+    if($aCount === strlen($s)) {
+      return $n;
+    }
+
+    $totalCount = 0;
+    $repeatTimes = ceil($n/strlen($s));
+    $newStr = str_repeat($s, $repeatTimes);
+
+    for($x = 0; $x < $n; $x++) {
+      if($newStr[$x] === "a") {
+        $totalCount++;
+      }
+    }
+    return $totalCount;
   }
 
   public static function mirrorSequence($a, $b)
   {
     // TODO
+    $str = "";
+    
+    for($x = $a; $x <= $b; $x++) {
+      $str = $str . $x;
+    }
+
+    for($x = $b; $x >= $a; $x--) {
+      $str = $str . strrev($x);
+    }
+
+    return $str;
   }
 
   public static function zeroMeansZero($a, $b)
   {
     // TODO
+    //$str = $a+$b . "";
+    return str_replace ("0" , "", $a+$b . "");
   }
 
   public static function numberOfLeds($number)
   {
     // TODO
+    $dict = [1 => 2, 
+             2 => 5,
+             3 => 5,
+             4 => 4,
+             5 => 5,
+             6 => 6,
+             7 => 3,
+             8 => 7,
+             9 => 6,
+             0 => 6];
+
+    $num = 0;
+
+    for($x = 0; $x < strlen($number); $x++) {
+      $num += $dict[intval($number[$x])];
+    }
+    return $num;
   }
 }
--- interviewPreparationKit/src/Vector/ArrayUtil.php
+++ interviewPreparationKit/src/Vector/ArrayUtil.php
@@ -11,21 +11,85 @@ class ArrayUtil
 
   public function rotLeft($d)
   {
-
+    $arr = [];
+    foreach($this->array as $index=>$value){
+      $newIndex = $index - $d;
+      $actualIndex = ($newIndex < 0 ? $newIndex + sizeof($this->array) : $newIndex);
+      $arr[$actualIndex] = $value;
+    }
+    return $arr;
   }
 
   public function stats()
-  {
-  
+  { 
+    $proportionArr = [];
+    $biggerZero = 0;
+    $zeros = 0;
+    $smallerZero = 0;
+
+    foreach($this->array as $value) {
+      if($value > 0) { 
+        $biggerZero++;
+      }elseif($value < 0) {
+        $smallerZero++;
+      }else{
+        $zeros++;
+      }
+    }
+
+    $proportionArr[] = number_format(($biggerZero/sizeof($this->array)), 6);
+    $proportionArr[] = number_format(($smallerZero/sizeof($this->array)), 6);
+    $proportionArr[] = number_format(($zeros/sizeof($this->array)), 6);
+
+    return $proportionArr;
   }
 
   public function miniMaxSum()
   {
-    
+    $finalArr = [];
+    sort($this->array);
+    $tempSum = 0;
+
+    for($x = 0; $x < sizeof($this->array) - 1; $x++) {
+      $tempSum += $this->array[$x];
+    }
+
+    $finalArr[] = $tempSum;
+    $tempSum = 0;
+
+    for($x = 1; $x < sizeof($this->array); $x++) {
+      $tempSum += $this->array[$x];
+    }
+
+    $finalArr[] = $tempSum;
+
+    return $finalArr;
   }
+
 
   public function highestFrequency()
   {
-    
+    $seenArr = [];
+
+    foreach($this->array as $number) {
+      if(array_key_exists($number, $seenArr)) {
+        $seenArr[$number] = $seenArr[$number] += 1;
+      } else {
+        $seenArr[$number] = 1;
+      }
+    }
+
+    $finalNumber = 0;
+    $index = array_keys($seenArr)[0];
+    $biggerNum = $seenArr[$index];
+
+    foreach($seenArr as $index=>$number) {
+      if($biggerNum < $number) {
+        $biggerNum = $number;
+        $finalNumber = $index;
+      }
+    }
+
+    return $finalNumber;
   }
 }
